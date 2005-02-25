0d55028ad706d44eb8f1200368a08078da482f33
bishop <bishop@d3944efe-84e1-0310-922a-b45035249031>
2005-02-25 00:07:26 +0000

    more talking in the installer
    git-svn-id: https://modzer0.cs.uaf.edu/svn/bcode/trunk/yum@216 d3944efe-84e1-0310-922a-b45035249031

--- centos4/install.sh
+++ centos4/install.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+echo "moving /etc/yum.conf to /etc/yum.conf.default"
 mv /etc/yum.conf /etc/yum.conf.default
+echo "moving /etc/yum.repos.d to /etc/yum.repos.default"
 mv /etc/yum.repos.d /etc/yum.repos.default
+echo "Installing cleaner yum.conf"
 cp yum.conf /etc/yum.conf
+echo "Putting version tracked yum.repos.d in place"
 cp -r . /etc/yum.repos.d
 
 echo "Now go to /etc/yum.repos.d and import any GPG keys you need"
 echo "See the comments in each repo for the import command/url to GPG key"
+
+echo 
+echo "you can stay in sync w/ bishop's yum.repos.d by going to"
+echo "/etc/yum.repos.d and typing: svn update"
+echo
+echo "Enjoy! .bishop"
+
