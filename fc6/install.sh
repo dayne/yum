86107e49a2095bf47ea7b81f737447278db84869
bishop <bishop@d3944efe-84e1-0310-922a-b45035249031>
2006-10-29 21:34:23 +0000

    fedora core 6 yum repositories.. livna testing
    git-svn-id: https://modzer0.cs.uaf.edu/svn/bcode/trunk/yum@637 d3944efe-84e1-0310-922a-b45035249031

---
+++ fc6/install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ -f ./yum.conf ] ; then
+	if [ -f /etc/yum.conf.default ] ; then
+		echo "/etc/yum.conf.default detected"
+		echo "aborting.... clean this up yourself"
+		exit
+	fi
+	echo "moving /etc/yum.conf to /etc/yum.conf.default"
+	mv /etc/yum.conf /etc/yum.conf.default
+	echo "Installing cleaner yum.conf"
+	cp yum.conf /etc/yum.conf
+fi
+
+if [ -d /etc/yum.repos.default ] ; then
+        echo "/etc/yum.repos.default detected"
+        echo "aborting.... clean this up yourself"
+        exit;
+fi
+
+echo "moving /etc/yum.repos.d to /etc/yum.repos.default"
+mv /etc/yum.repos.d /etc/yum.repos.default
+
+echo "Putting version tracked yum.repos.d in place"
+cp -r . /etc/yum.repos.d
+
+echo "installing the handy bash-completion"
+yum install -y bash-completion
+
+echo 
+echo "you can stay in sync w/ bishop's yum.repos.d by going to"
+echo "/etc/yum.repos.d and typing: svn update"
+echo
+echo "Enjoy! .bishop"
