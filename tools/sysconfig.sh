74ff1e5f50d1c00aea29d94367e84d15b379c797
root <root@electron.gina.alaska.edu>
2010-08-04 07:53:15 +0800

    updated sysconfig to point at new ks.gina server, removed things I wont be needing

--- tools/sysconfig.sh
+++ tools/sysconfig.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# written for Centos 5.4 x86_64
+# written for Centos 5.5 x86_64
 # Script to automate various system configurations on new builds
 
 
 # set the timezone
 echo "setting timezone to Alaskan start ntpd"
 mv /etc/localtime /etc/localtime-default
-ln -s /usr/share/zoneinfo/America/Anchorage /etc/localtime
+cp /usr/share/zoneinfo/America/Anchorage /etc/localtime
 
 echo 'ZONE="America/Anchorage"' > /etc/sysconfig/clock
 echo 'UTC=false' >> /etc/sysconfig/clock
@@ -29,36 +29,20 @@ cd yum/centos5/
 yum -y update
 popd
 
-# useful libraries that Dan suggested
-#yum -y install GraphicsMagick-perl\
-#   gdal-perl\
-#   perl-Class-Accessor\
-#   perl-DBD-Pg\
-#   perl-IPC-Run3\
-#   perl-Inline\
-#   perl-LockFile-Simple\
-#   perl-Mail-Sendmail\
-#   perl-Math-MatrixReal\
-#   perl-Math-VectorReal\
-#   perl-Parse-RecDescent\
-#   perl-Template-Toolkit\
-#   perl-XML-Writer perl-YAML-Syck
-
 echo "install and configure denyhosts"
 yum -y install denyhosts
 ln -s /usr/share/doc/denyhosts-2.6/daemon-control-dist /etc/init.d/denyhosts
 /usr/share/doc/denyhosts-2.6/denyhosts.py -c /usr/share/doc/denyhosts-2.6/denyhosts.cfg-dist
 cp /usr/share/doc/denyhosts-2.6/denyhosts.cfg-dist /usr/share/denyhosts/denyhosts.cfg
-wget -O /usr/share/denyhosts/denyhosts.cfg http://nori.gina.alaska.edu/sysconfig/denyhosts.cfg 
+wget -O /usr/share/denyhosts/denyhosts.cfg http://ks.gina.alaska.edu/denyhosts.cfg 
 
 echo "ALL: 137.229.19.0/255.255.255.0" >> /etc/hosts.allow
 
 /etc/init.d/denyhosts start
 chkconfig denyhosts on
 
-
 echo "setup root mail forwarding"
-wget -O /etc/mail/sendmail.mc http://nori.gina.alaska.edu/sysconfig/sendmail.mc
+wget -O /etc/mail/sendmail.mc http://ks.gina.alaska.edu/sendmail.mc
 
 yum -y install sendmail-cf
 make -C /etc/mail
@@ -72,38 +56,23 @@ echo "sendmail test from `ifconfig eth0 | grep 'inet addr' | awk '{print $2}'`" 
 
 sleep 1
 echo "automatic yum updates"
-wget -O /etc/yum/yum-updatesd.conf http://nori.gina.alaska.edu/sysconfig/yum-updatesd.conf
+wget -O /etc/yum/yum-updatesd.conf http://ks.gina.alaska.edu/yum-updatesd.conf
 
 service yum-updatesd start
 chkconfig yum-updatesd on
 
 # install Ruby and Gems
-# wget http://tofu.gina.alaska.edu/ruby.sh
+ #wget http://ks.gina.alaska.edu/ruby.sh
 # sh ruby.sh /usr/local 1
 
-#wget http://nori.gina.alaska.edu/sysconfig/rubygems-1.3.5.tgz
+#wget http://ks.gina.alaska.edu/rubygems-1.3.5.tgz
 #tar zxfv rubygems-1.3.5.tgz
 #cd rubygems-1.3.5
 #ruby setup.rb
 
 #gem install open4 rails mongrel cheat facets net-ssh net-ping crypt-fog daemons net-sftp highline GeoRuby xmpp4r-simple RingyDingy vim-ruby hpricot postgres-pr
 
-#get git!
-#cd
-#wget http://nori.gina.alaska.edu/sysconfig/git-1.6.3.2.tar.gz
-#tar zxfv git-1.6.3.2.tar.gz
-#cd git-1.6.3.2/
-#./configure
-#make
-#make install
 yum install -y git
-
-#install memtest
-# memtest not needed for VM's
-if [ "" == "`grep -i qemu /proc/cpuinfo`" ]; then
-  yum -y install memtest86+
-  memtest-setup
-fi
 
 echo "set up DNS"
 # TODO: perhaps revamp this and figure out if this is appropriate
@@ -113,22 +82,21 @@ echo "nameserver 137.229.31.16" >> /etc/resolv.conf
 echo "nameserver 137.229.10.39" >> /etc/resolv.conf
 echo "nameserver 137.229.30.33" >> /etc/resolv.conf
 
-
 # ------------ Configure NRPE (Nagios Plugin) ------------ #
 echo "setting up NRPE..."
 
-yum -y install nagios-nrpe nagios-plugins-all xinetd
-wget -O /etc/xinetd.d/nrpe http://nori.gina.alaska.edu/sysconfig/nrpe
+yum -y install nagios-nrpe nagios-plugins xinetd yum nagios-plugins-users nagios-plugins-procs nagios-plugins-disk nagios-plugins-http nagios-plugins-load
+
+wget -O /etc/xinetd.d/nrpe http://ks.gina.alaska.edu/nrpe
+wget -O /etc/nagios/nrpe.cfg http://ks.gina.alaska.edu/nrpe.cfg
 echo " nrpe               5666/tcp           # NRPE" >> /etc/services
 chkconfig xinetd on
 chkconfig nrpe on
 /etc/init.d/xinetd start
 service nrpe start
 
-# set up firewall exceptions
-iptables -A INPUT -p tcp --dport 5666 -j ACCEPT
-service iptables restart
-
 echo ""
 
 echo "add this to /etc/mdadm.conf...  MAILADDR alert@gina.alaska.edu"
+echo "and add this to iptables:"
+echo "-A RH-Firewall-1-INPUT -p tcp -m tcp --dport 5666 -j ACCEPT"
